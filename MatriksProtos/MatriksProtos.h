d309b88d31f2da8a20ae8e31e162fa3e3f1e2f01
Ali Mert Ozdemir (Garanti Teknoloji) <AliOzdem@garantibbva.com.tr>
2022-03-09 02:05:00 +0300

    Initial Commit

---
+++ MatriksProtos/MatriksProtos.h
@@ -0,0 +1,18 @@
+//
+//  MatriksProtos.h
+//  MatriksProtos
+//
+//  Created by AliOzdem on 9.03.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MatriksProtos.
+FOUNDATION_EXPORT double MatriksProtosVersionNumber;
+
+//! Project version string for MatriksProtos.
+FOUNDATION_EXPORT const unsigned char MatriksProtosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MatriksProtos/PublicHeader.h>
+
+
